909f94d329924f8e7af4bac741314d761a1723cf
leguass7 <leandro.sbrissa@hotmail.com>
2020-03-27 21:07:21 -0300

    feat: add TextOnOff

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export { default as globalTheme } from './GlobalStyle/globalTheme';
 export { imgMessage, imgIcon, imgForm, imgButton, getKeyImage } from './Images';
 export { ImageMessage } from './ImageMessage';
 export { CircleLoading, Carga } from './Loading';
-export { Nota, Text, TextOff, TextOn, Span, PBloco } from './Texts';
+export { Nota, Text, TextOnOff, TextOff, TextOn, Span, PBloco } from './Texts';
 export { default as BoxCenter } from './BoxCenter';
 export { default as PageTitle } from './PageTitle';
 export { ListCards, CardContent, CardItem, CardTitle } from './ListCards';
