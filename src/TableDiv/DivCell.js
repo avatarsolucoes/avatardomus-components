7d75bb366da26105e16254fca492bfc42353f536
leguass7 <leandro.sbrissa@hotmail.com>
2020-12-18 11:56:30 -0300

    feat: added grow divcel

--- src/TableDiv/DivCell.js
+++ src/TableDiv/DivCell.js
@@ -4,12 +4,19 @@ import cx from 'classnames';
 import css from './tablediv.css';
 
 export default function DivCell(props) {
-  const { children, className, style, width, ...rest } = props;
+  const { children, className, style, width, grow, ...rest } = props;
   const classCell = cx(css.tabledivCell, 'table-div-cell', className);
   const styles = { ...style };
   if (width) {
     styles.width = width;
     styles.maxWidth = width;
+  }
+  if (grow) {
+    if (typeof grow === 'number') {
+      styles.flexGrow = grow;
+    } else {
+      styles.flexGrow = 1;
+    }
   }
   return (
     <div {...rest} className={classCell} style={styles}>
@@ -22,5 +29,6 @@ DivCell.propTypes = {
   className: PropTypes.string,
   style: PropTypes.object,
   width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  grow: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
 };
-DivCell.defaultProps = { children: null, className: null, style: {}, width: null };
+DivCell.defaultProps = { children: null, className: null, style: {}, width: null, grow: null };
